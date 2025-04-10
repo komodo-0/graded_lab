31303aa5b823a1388ccf634ef7935fb43912eb19
Jérémie <jeremie.otto@gmail.com>
2025-04-10 15:41:24 +0200

    ajout des tests

--- main-test.py
+++ main-test.py
@@ -1,26 +1,57 @@
 import pytest
+from constructs import Construct
 from cdktf import Testing
-
-# The tests below are example tests, you can find more information at
-# https://cdk.tf/testing
-
+from main import LambdaStack
 
 class TestMain:
 
     def test_my_app(self):
         assert True
 
-    #stack = TerraformStack(Testing.app(), "stack")
-    #app_abstraction = MyApplicationsAbstraction(stack, "app-abstraction")
-    #synthesized = Testing.synth(stack)
+    def test_should_contain_lambda_function(self, tmp_path):
+        app = Testing.app()
+        stack = LambdaStack(app, "test")
+        synth = Testing.synth(stack)
 
-    # def test_should_contain_container(self):
-    #    assert Testing.to_have_resource(self.synthesized, Container.TF_RESOURCE_TYPE)
+        assert synth.get_resource_count("aws_lambda_function") == 1
+        assert synth.get_by_path("aws_lambda_function.lambda")["properties"]["function_name"] == "calculatrice"
 
-    # def test_should_use_an_ubuntu_image(self):
-    #    assert Testing.to_have_resource_with_properties(self.synthesized, Image.TF_RESOURCE_TYPE, {
-    #        "name": "ubuntu:latest",
-    #    })
+    def test_should_use_python39_runtime(self, tmp_path):  
+        app = Testing.app()
+        stack = LambdaStack(app, "test")
+        synth = Testing.synth(stack)
 
-    # def test_check_validity(self):
-    #    assert Testing.to_be_valid_terraform(Testing.full_synth(stack))
+        assert synth.get_by_path("aws_lambda_function.lambda")["properties"]["runtime"] == "python3.9"
+
+    def test_should_contain_input_queue(self, tmp_path):
+        app = Testing.app()
+        stack = LambdaStack(app, "test")
+        synth = Testing.synth(stack)
+
+        assert synth.get_resource_count("aws_sqs_queue") == 2
+        assert synth.get_by_path("aws_sqs_queue.input_queue")["properties"]["name"] == "input_queue"
+
+    def test_should_contain_output_queue(self, tmp_path):
+        app = Testing.app()
+        stack = LambdaStack(app, "test")
+        synth = Testing.synth(stack)
+
+        assert synth.get_resource_count("aws_sqs_queue") == 2
+        assert synth.get_by_path("aws_sqs_queue.output_queue")["properties"]["name"] == "output_queue"
+
+    def test_should_link_lambda_to_input_queue(self, tmp_path):
+        app = Testing.app()
+        stack = LambdaStack(app, "test")
+        synth = Testing.synth(stack)
+
+        assert synth.get_resource_count("aws_lambda_event_source_mapping") == 1
+        assert synth.get_by_path("aws_lambda_event_source_mapping.event_source_mapping")["properties"]["event_source_arn"] == "${aws_sqs_queue.input_queue.arn}"
+        assert synth.get_by_path("aws_lambda_event_source_mapping.event_source_mapping")["properties"]["function_name"] == "${aws_lambda_function.lambda.arn}"
+
+    def test_should_set_environment_variable_for_output_queue(self, tmp_path):
+        app = Testing.app()
+        stack = LambdaStack(app, "test")
+        synth = Testing.synth(stack)
+
+        env_vars = synth.get_by_path("aws_lambda_function.lambda")["properties"]["environment"]["variables"]
+        assert env_vars["OUTPUT_QUEUE_URL"] == "${aws_sqs_queue.output_queue.url}"
